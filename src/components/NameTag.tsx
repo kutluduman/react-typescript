720ae9830116d61aab61822250efb69462845c5b
kutluduman <kutluduman@gmail.com>
2021-07-05 12:27:47 -0400

    Create NameTag component and passed props via typescript format

---
+++ src/components/NameTag.tsx
@@ -0,0 +1,20 @@
+import * as PropTypes from "prop-types";
+
+type NameTagProps = {
+  name: String
+}
+
+const NameTag = (props: NameTagProps) => {
+  return (
+    <main>
+      <header>
+        <h1>Hello</h1>
+        <p>My Name Is</p>
+      </header>
+      <section className="display-name">
+          <p>{props.name}</p>
+      </section>
+    </main>
+  )
+};
+export default NameTag;
